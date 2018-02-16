28323802fd1cc3136a6d9b2f4340c4a49954fc11
Charles <dailiang18bb@gmail.com>
2018-02-16 11:41:20 -0500

    Working on the java file to get the data from xml

--- CS639SpringHW2/app/src/main/java/com/pace/cs639spring/hw2/AnimalDisplayListViewAdapter.java
+++ CS639SpringHW2/app/src/main/java/com/pace/cs639spring/hw2/AnimalDisplayListViewAdapter.java
@@ -1,18 +1,42 @@
 package com.pace.cs639spring.hw2;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * Created by kachi on 2/7/18.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
+class singleRow {
+
+    String mDescription;
+    int mImage;
+
+}
+
 
 public class AnimalDisplayListViewAdapter extends BaseAdapter {
+    Context mContext;
+    ArrayList <singleRow> mList;
 
+    AnimalDisplayListViewAdapter(Context context){
+        mList = new ArrayList<singleRow>();
+        mContext = context;
 
+        Resources res = context.getResources();
+        String[] descriptions = res.getStringArray(R.array.deer_description);
+        int[] images = {R.drawable.ic_bat, R.drawable.ic_bird, R.drawable.ic_butterfly, R.drawable.ic_cat, R.drawable.ic_deer, R.drawable.ic_dog, R.drawable.ic_duck};
 
-
+        /*
+        * 1. get values from xml
+        *
+        */
+    }
 
 
 
@@ -34,5 +58,61 @@ public class AnimalDisplayListViewAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         return null;
+    }
+}
+
+
+public class TestAdapter extends BaseAdapter {
+
+    Context mContext;
+    List<ExampleObject> mExamples;
+
+    TestAdapter(Context context, List<ExampleObject> exampleObjectList) {
+        mContext = context;
+        mExamples = exampleObjectList;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = View.inflate(mContext, R.layout.list_item, null);
+            ViewHolder viewHolder = new ViewHolder((ImageView) convertView.findViewById(R.id.image),
+                    (TextView) convertView.findViewById(R.id.text));
+            convertView.setTag(viewHolder);
+        }
+
+        ExampleObject object = (ExampleObject) getItem(position);
+        ViewHolder viewHolder = (ViewHolder) convertView.getTag();
+
+        viewHolder.mImageView.setBackgroundColor(object.mColor);
+        viewHolder.mTextView.setText(object.mName);
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return mExamples.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mExamples.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    static class ViewHolder {
+        ImageView mImageView;
+        TextView mTextView;
+
+        ViewHolder(ImageView imageView, TextView textView) {
+            mImageView = imageView;
+            mTextView = textView;
+        }
+
     }
 }
