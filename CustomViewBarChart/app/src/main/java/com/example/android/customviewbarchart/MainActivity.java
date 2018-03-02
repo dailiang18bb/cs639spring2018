12f4862fb1a92bde111b27ce9127fed284a33d81
Charles <dailiang18bb@gmail.com>
2018-03-02 15:24:15 -0500

    update

--- CustomViewBarChart/app/src/main/java/com/example/android/customviewbarchart/MainActivity.java
+++ CustomViewBarChart/app/src/main/java/com/example/android/customviewbarchart/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.customviewbarchart;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mCharlesView = findViewById(R.id.charles_view);
-
     }
+
+    public void addData (View view){ mCharlesView.add();}
 }
